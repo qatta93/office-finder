a65505d3924e98a37d4d6519fa6967624c91fcd8
qatta93 <panasiuk.patrycja@gmail.com>
2022-09-11 19:34:23 +0200

    responsive about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -24,16 +24,34 @@ const About: NextPage = () => {
         <h1>ABOUT</h1>
         <Wrapper>
           <Notepad>
-            <Image src='/images/notepad.png' alt='notepad' width='300px' height='300px' className='notebook__img'></Image>
+            <Image src='/images/notepad.png' alt='notepad' layout="fill"></Image>
             <p>Office Finder can help you with finding comfortable space to work! Are you traveling? Don't you have a comfy desk at home? Check our <Link href='/maps' className='link'>maps</Link> to find offices close to you.</p>
           </Notepad>
-          {windowWidth && windowWidth > 1023 && <Image src='/images/designer.png'  alt='designer' width='300px' height='300px' className='designer__img'></Image>}
+          {windowWidth && windowWidth > 1023 
+          && 
+          <Graphic>
+            <Image src='/images/designer.png'  alt='designer' layout="fill"></Image>
+          </Graphic>}
         </Wrapper>
     </Section>
   )
 }
 
 export default About;
+
+const Graphic = styled.section`
+  padding: 0 3rem 0 0;
+  position: relative;
+  height: 300px;
+  min-width: 30vw;
+  max-width: 40vw;
+  margin: auto 0;
+  opacity: 0.7;
+
+  @media only screen and (min-width: 1200px) {
+    height: 400px;
+  }
+`
 
 const Section = styled.section`
   padding: 0.5rem;
@@ -48,11 +66,14 @@ const Section = styled.section`
     font-size: 1.7rem;
     color: #E1FDFF;
   }
+
+  @media only screen and (min-width: 1200px) {
+    padding: 0.5rem 3rem;
+  }
 `
 const Wrapper = styled.section`
-.designer__img {
-    opacity: 0.5;
-}
+  width: 100%;
+  height: 400px;
 
 @media only screen and (min-width: 1024px) {
   display: flex;
@@ -69,15 +90,32 @@ const Notepad = styled.section`
   display: flex;
   align-items: center;
   justify-content: center;
+  height: 300px;
+  min-width: 50vw;
+  max-width: 70vw;
+  margin: 0 auto;
+  
 
 
   @media only screen and (min-width: 578px) {
     margin-top: 4rem;
+    height: 400px;
+    min-width: 50vw;
+    max-width: 60vw;
+  }
+  @media only screen and (min-width: 768px) {
+    height: 450px;
   }
   @media only screen and (min-width: 1024px) {
     margin-top: 0rem;
-    .notebook__img {
-    }
+    height: 330px;
+    min-width: 40vw;
+    max-width: 50vw;
+  }
+  @media only screen and (min-width: 1200px) {
+    margin-top: 2rem;
+    height: 450px;
+  }
   }
 
 
@@ -87,7 +125,7 @@ const Notepad = styled.section`
     font-weight: 600;
     position: absolute;
     top: 50%;
-    left: 50%;
+    left: 45%;
     transform: translate(-50%, -50%);
     width: 180px;
     text-align: center justify;
@@ -103,12 +141,27 @@ const Notepad = styled.section`
       font-size: 1.3rem;
       line-height: 1.7rem;
     }
+    @media only screen and (min-width: 578px) {
+      width: 300px;
+      left: 44%;
+      font-size: 1.5rem;
+      line-height: 1.9rem;
+    }
     @media only screen and (min-width: 768px) {
-      left: 49%;
+      left: 45%;
+      font-size: 1.7rem;
+      line-height: 2.2rem;
     }
     @media only screen and (min-width: 1024px) {
-      font-size: 1rem;
-      width: 200px
+      width: 300px;
+      font-size: 1.4rem;
+      line-height: 2rem;
+    }
+    @media only screen and (min-width: 1200px) {
+      left: 43%;
+      width: 350px;
+      font-size: 1.6rem;
+      line-height: 2.2rem;
     }
 
     &>* {
